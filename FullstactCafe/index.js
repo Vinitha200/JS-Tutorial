3083eb708d09b91c82788278c588ec8f15d5beaa
Vinitha200 <vinithashanmugam200@gmail.com>
2023-11-03 23:16:06 +0530

    fullstack cafe

--- FullstactCafe/index.js
+++ FullstactCafe/index.js
@@ -26,3 +26,35 @@ function reverse_Sentence(str){
     return res.join(" ")
 }
 console.log(reverse_Sentence('Welcome to this Javascript Guide!'))
+
+//14.Find the missing one 
+// The output of the function should be 8
+// var arrayOfIntegers = [2, 5, 1, 4, 9, 6, 3, 7];
+// var upperBound = 9;
+// var lowerBound = 1;
+
+function missing_brute(arr,lowerBound,upperBound){
+    let n = arr.length
+    for(let i=lowerBound;i<=upperBound;i++){
+        let flag = 0
+        for(let j=0;j<n;j++){
+             if(arr[j]===i){
+                flag=1
+                break
+             }
+        }
+       if(flag===0){
+        return i
+       } 
+    }
+}
+console.log(missing_brute([2, 5, 1, 4, 9, 6, 3, 7],1,9))
+
+function missing_optimal(arr,lowerBound,upperBound){
+       
+    //here n is upperbound
+    let sumation = (upperBound*(upperBound+1))/2
+    let sumarr = arr.reduce((acc,curr)=> acc+curr ,0)
+    return sumation-sumarr
+}
+console.log(missing_brute([2, 5, 1, 4, 9, 6, 3, 7],1,9))
